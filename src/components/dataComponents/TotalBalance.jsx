ad0c4b5c39b52c8abce0873442cdad6996ccd3b4
Tomas <tomas.casabona@gmail.com>
2024-06-03 14:22:16 +0200

    get total done

--- src/components/dataComponents/TotalBalance.jsx
+++ src/components/dataComponents/TotalBalance.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { getTotal } from "../../services/getTotal";
 
 export const TotalBalance = () => {
   const data = useSelector((state) => state.data.weekData);
-  console.log(data);
+  const total = getTotal({ data });
   return (
     <div>
       <div>lunes {data.lunes}</div>
       <div>martes {data.martes}</div>
       <div>miercoles {data.miercoles}</div>
+      <div>total : {total}</div>
     </div>
   );
 };
