54cbb43bef321e8264d252ec4e7f545be1470c6e
Tomas <tomas.casabona@gmail.com>
2024-06-03 20:42:27 +0200

    test getDataPorcentage

--- src/services/getPercentageDifference.test.js
+++ src/services/getPercentageDifference.test.js
@@ -1,7 +1,7 @@
-import { describe, expect } from "vitest";
+import { describe, it, expect } from "vitest";
 import { getPercentageDifference } from "./getPercentageDifference";
 
-describe("getTotal", () => {
+describe("getPercentageDifference", () => {
   it("should be a function", () => {
     expect(typeof getPercentageDifference).toBe("function");
   });
@@ -10,15 +10,22 @@ describe("getTotal", () => {
     expect(() => getPercentageDifference("a", 2)).toThrow(
       "both parameters must be numbers"
     );
-  });
-
-  it("should return a number", () => {
-    expect(typeof getPercentageDifference(3, 3)).toBe(
-      "number"
+    expect(() => getPercentageDifference(2, "b")).toThrow(
+      "both parameters must be numbers"
+    );
+    expect(() => getPercentageDifference("a", "b")).toThrow(
+      "both parameters must be numbers"
     );
   });
 
-  it("should return the porcentual difference betwueen numbers", () => {
-    expect(getPercentageDifference(1,2)).toBe(100);
+  it("should return a number", () => {
+    expect(typeof getPercentageDifference(3, 3)).toBe("number");
+  });
+
+  it("should return the correct percentage difference between numbers", () => {
+    expect(getPercentageDifference(1, 2)).toBe(100);
+    expect(getPercentageDifference(100, 150)).toBe(50);
+    expect(getPercentageDifference(50, 75)).toBe(50);
+    expect(getPercentageDifference(200, 100)).toBe(-50);
   });
 });
