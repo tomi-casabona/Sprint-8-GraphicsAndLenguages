64e6bb07e76e698f8d051bf03e5f70a3c85b1b63
Tomas <tomas.casabona@gmail.com>
2024-06-05 11:13:07 +0200

    the components weeklyReport and graphic have been modified to enable the comparison state

--- src/components/dataComponents/WeeklyGraphic.jsx
+++ src/components/dataComponents/WeeklyGraphic.jsx
@@ -9,21 +9,34 @@ import {
   Tooltip,
   Legend,
 } from "chart.js";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  setTodayData,
+  setYesterdayData,
+} from "../../redux/slices/compareSlice";
 
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 export const WeeklyGraphic = () => {
+  const dispatch = useDispatch();
   const weekData = useSelector((state) => state.data.weekData);
-  const [selectedIndex, setSelectedIndex] = useState(null);  
+  const [selectedIndex, setSelectedIndex] = useState(null);
+
+  const days = Object.keys(weekData);
 
   const handleClick = (index) => {
     setSelectedIndex(index);
-    const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-    const selectedDay = days[index];
-    const previousDay = days[index - 1] || days[days.length - 1]; 
-    //setSelectedValue(weekData[selectedDay]);
-    //setPreviousValue(weekData[previousDay]);
+    const today = days[index];
+    const yesterday = index === 0 ? null : days[index - 1];
+    dispatch(setTodayData({ day: today, value: weekData[today] }));
+    dispatch(setYesterdayData({ day: yesterday, value: weekData[yesterday] }));
   };
 
   const options = {
@@ -35,33 +48,40 @@ export const WeeklyGraphic = () => {
     },
     onClick: (event, elements) => {
       if (elements.length > 0) {
-        handleClick(elements[0].index);
+        const index = elements[0].index;
+        handleClick(index);
       }
     },
   };
 
   const BarChartData = {
-    labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+    labels: days,
     datasets: [
       {
         label: "Expenses",
-        data: [
-          weekData.Monday,
-          weekData.Tuesday,
-          weekData.Wednesday,
-          weekData.Thursday,
-          weekData.Friday,
-          weekData.Saturday,
-          weekData.Sunday,
-        ],
+        data: days.map((day) => weekData[day]),
         backgroundColor: [
-          selectedIndex === 0 ? "rgba(255, 99, 123, .9)" : "rgba(255, 99, 123, 0.4)",
-          selectedIndex === 1 ? "rgba(155, 99, 123, .9)" : "rgba(155, 99, 123, 0.4)",
-          selectedIndex === 2 ? "rgba(55, 99, 123, .9)" : "rgba(55, 99, 123, 0.4)",
-          selectedIndex === 3 ? "rgba(0, 99, 123, .9)" : "rgba(0, 99, 123, 0.4)",
-          selectedIndex === 4 ? "rgba(255, 50, 0, .9)" : "rgba(255, 50, 0, 0.4)",
-          selectedIndex === 5 ? "rgba(255, 155, 0, .9)" : "rgba(255, 155, 0, 0.4)",
-          selectedIndex === 6 ? "rgba(255, 99, 200, .9)" : "rgba(255, 99, 200, 0.4)",
+          selectedIndex === 0
+            ? "rgba(255, 99, 123, .9)"
+            : "rgba(255, 99, 123, 0.4)",
+          selectedIndex === 1
+            ? "rgba(155, 99, 123, .9)"
+            : "rgba(155, 99, 123, 0.4)",
+          selectedIndex === 2
+            ? "rgba(55, 99, 123, .9)"
+            : "rgba(55, 99, 123, 0.4)",
+          selectedIndex === 3
+            ? "rgba(0, 99, 123, .9)"
+            : "rgba(0, 99, 123, 0.4)",
+          selectedIndex === 4
+            ? "rgba(255, 50, 0, .9)"
+            : "rgba(255, 50, 0, 0.4)",
+          selectedIndex === 5
+            ? "rgba(255, 155, 0, .9)"
+            : "rgba(255, 155, 0, 0.4)",
+          selectedIndex === 6
+            ? "rgba(255, 99, 200, .9)"
+            : "rgba(255, 99, 200, 0.4)",
         ],
         borderColor: [
           "rgba(255, 99, 123, .9)",
@@ -87,7 +107,9 @@ export const WeeklyGraphic = () => {
     ],
   };
 
-  return (    
-      <Bar options={options} data={BarChartData} />  
+  return (
+    <div className="w-2/6">
+      <Bar options={options} data={BarChartData} />;
+    </div>
   );
 };
--- src/components/dataComponents/WeeklyReport.jsx
+++ src/components/dataComponents/WeeklyReport.jsx
@@ -3,15 +3,18 @@ import { WeeklyGraphic } from "./WeeklyGraphic";
 import { TotalBalance } from "./TotalBalance";
 import { VariationWithYesterday } from "./VariationWithYesterday";
 import { TodaySpentBalance } from "./TodaySpentBalance";
+import { useSelector } from "react-redux";
 
 export const WeeklyReport = () => {
+  const { today } = useSelector((state) => state.compare.compareData);
+  console.log(today + " desde el state");
   return (
     <div className="m-4">
       <TotalBalance />
       <WeeklyGraphic />
       <div className="flex flex-row justify-between">
-        <TodaySpentBalance/>
-      <VariationWithYesterday today={5} yesterday={2}/>
+        <TodaySpentBalance />
+        <VariationWithYesterday today={5} yesterday={2} />
       </div>
     </div>
   );
